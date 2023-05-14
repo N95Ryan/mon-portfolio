c5410ea7b5107a9e0a4d3e6bfa355f563eaef3e6
Ryan <ryanpinas77@gmail.com>
2023-05-14 14:05:10 +0200

    Auto typing added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     email: "ryanpinas77@gmail.com",
     status: "Étudiant en développement web",
   };
-
   return (
     <>
       <Header />
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,65 +1,66 @@
-import Typewriter from "typewriter-effect";
+import React from "react";
 import SocialIcons from "../../components/SocialIcons";
+import Typed from "typed.js";
+
+const styles = {
+  landing: {
+    height: "calc(100% - 93px)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  
+  textContainer: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    letterSpacing: "1px",
+    textAlign: "center",
+    zIndex: "1",
+    color: "#fff",
+    textShadow: "1px 1px 3px #000",
+  },
+  
+  name: {
+    color: "#fff",
+    fontWeight: "700",
+    marginTop: "-100px",
+    paddingBottom: "28px",
+  },
+};
 
 const Landing = ({ name }) => {
-  const styles = {
-    landing: {
-      height: "calc(100% - 93px)",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    },
+  const AutoType = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(AutoType.current, {
+      strings: [
+        "Développeur web",
+        "Étudiant en informatique",
+        "Amateur de pop culture",
+      ],
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop: Infinity,
+    });
 
-    landingImage: {
-      position: "absolute",
-      bottom: "0",
-      opacity: "0.3",
-      mixBlendMode: "lighten",
-      height: "80%",
-    },
-
-    textContainer: {
-      display: "flex",
-      flexDirection: "column",
-      letterSpacing: "1px",
-      textAlign: "center",
-      zIndex: "1",
-      color: "#fff",
-      textShadow: "1px 1px 3px #000",
-    },
-
-    name: {
-      color: "#fff",
-      fontWeight: "700",
-      marginTop: "-100px",
-      paddingBottom: "28px",
-    },
-  };
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
-    <section className="landing" style={styles.landing}>
+    <section className="landing flex flex-col items-center justify-center" style={styles.landing}>
       <div className="textContainer" style={styles.textContainer}>
         <h1 className="name" style={styles.name}>
           {name}
         </h1>
-        <div className="description">
-          <Typewriter
-            className="description"
-            onInit={(typewriter) => {
-              typewriter
-                .typeString("Étudiant en informatique")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("Développeur web")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("UX Designer")
-                .pauseFor(1500)
-                .deleteAll()
-                .start();
-            }}
-          />
-        </div>
+      </div>
+      <div className="flex justify-center">
+        <p className="text-3xl text-thin text-purple-400">
+          <span ref={AutoType} />
+        </p>
       </div>
       <SocialIcons />
     </section>
